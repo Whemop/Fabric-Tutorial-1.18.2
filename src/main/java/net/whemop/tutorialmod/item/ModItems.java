04582914642e2cdbc31fa8fe2d9b40be8e7623b1
Whemop <spencenate1230@gmail.com>
2022-05-29 19:53:26 -0400

    Added Dowsing Rod

--- src/main/java/net/whemop/tutorialmod/item/ModItems.java
+++ src/main/java/net/whemop/tutorialmod/item/ModItems.java
@@ -6,6 +6,8 @@ import net.minecraft.item.ItemGroup;
 import net.minecraft.util.Identifier;
 import net.whemop.tutorialmod.TutorialMod;
 import net.minecraft.util.registry.Registry;
+import net.whemop.tutorialmod.item.custom.DowsingRodItem;
+import org.lwjgl.system.CallbackI;
 
 public class ModItems {
     // Mythril Ingot
@@ -17,6 +19,10 @@ public class ModItems {
     // Raw Mythril
     public static final Item RAW_MYTHRIL = registerItem("raw_mythril",
             new Item(new FabricItemSettings().group(ModItemGroup.MYTHRIL)));
+    // Dowsing Rod
+    public static final Item DOWNSING_ROD = registerItem("dowsing_rod",
+            new DowsingRodItem(new FabricItemSettings().group(ModItemGroup.MYTHRIL).maxDamage(16)));
+
 
     public static Item registerItem(String name, Item item) {
         return Registry.register(Registry.ITEM, new Identifier(TutorialMod.MOD_ID, name), item);
