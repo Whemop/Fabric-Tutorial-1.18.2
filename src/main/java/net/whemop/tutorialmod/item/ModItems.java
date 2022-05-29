476f348f93d48b822912362530f982c54a7828bc
Whemop <spencenate1230@gmail.com>
2022-05-29 15:50:58 -0400

    Added Raw Mythril

--- src/main/java/net/whemop/tutorialmod/item/ModItems.java
+++ src/main/java/net/whemop/tutorialmod/item/ModItems.java
@@ -8,12 +8,15 @@ import net.whemop.tutorialmod.TutorialMod;
 import net.minecraft.util.registry.Registry;
 
 public class ModItems {
+    // Mythril Ingot
     public static final Item MYTHRIL_INGOT = registerItem("mythril_ingot",
             new Item(new FabricItemSettings().group(ItemGroup.MISC)));
-
+    // Mythril Nugget
     public static final Item MYTHRIL_NUGGET = registerItem("mythril_nugget",
             new Item(new FabricItemSettings().group(ItemGroup.MISC)));
-
+    // Raw Mythril
+    public static final Item RAW_MYTHRIL = registerItem("raw_mythril",
+            new Item(new FabricItemSettings().group(ItemGroup.MISC)));
 
     public static Item registerItem(String name, Item item) {
         return Registry.register(Registry.ITEM, new Identifier(TutorialMod.MOD_ID, name), item);
