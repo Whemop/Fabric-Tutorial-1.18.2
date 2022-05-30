b8b42c09cfdae99c335770e54fc57f584f92f9e3
Whemop <spencenate1230@gmail.com>
2022-05-30 00:42:34 -0400

    Added Mythril Tools (Sword, Pickaxe, Axe, Shovel, Hoe)

--- src/main/java/net/whemop/tutorialmod/item/ModItems.java
+++ src/main/java/net/whemop/tutorialmod/item/ModItems.java
@@ -3,10 +3,15 @@ package net.whemop.tutorialmod.item;
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
+import net.minecraft.item.ShovelItem;
+import net.minecraft.item.SwordItem;
 import net.minecraft.util.Identifier;
 import net.whemop.tutorialmod.TutorialMod;
 import net.minecraft.util.registry.Registry;
 import net.whemop.tutorialmod.item.custom.DowsingRodItem;
+import net.whemop.tutorialmod.item.custom.ModAxeItem;
+import net.whemop.tutorialmod.item.custom.ModHoeItem;
+import net.whemop.tutorialmod.item.custom.ModPickaxeItem;
 import org.lwjgl.system.CallbackI;
 
 public class ModItems {
@@ -28,7 +33,21 @@ public class ModItems {
     // Grape
     public static final Item GRAPE = registerItem("grape",
             new Item(new FabricItemSettings().group(ModItemGroup.MYTHRIL).food(ModFoodComponents.GRAPE)));
-
+    // Mythril Sword
+    public static final Item MYTHRIL_SWORD = registerItem("mythril_sword",
+            new SwordItem(ModToolMaterials.MYTHRIL, 1, 2f, new FabricItemSettings().group(ModItemGroup.MYTHRIL)));
+    // Mythril Pickaxe
+    public static final Item MYTHRIL_PICKAXE = registerItem("mythril_pickaxe",
+            new ModPickaxeItem(ModToolMaterials.MYTHRIL, 2, 2f, new FabricItemSettings().group(ModItemGroup.MYTHRIL)));
+    // Mythril Axe
+    public static final Item MYTHRIL_AXE = registerItem("mythril_axe",
+            new ModAxeItem(ModToolMaterials.MYTHRIL, 3, 1f, new FabricItemSettings().group(ModItemGroup.MYTHRIL)));
+    // Mythril Shovel
+    public static final Item MYTHRIL_SHOVEL = registerItem("mythril_shovel",
+            new ShovelItem(ModToolMaterials.MYTHRIL, 1, 2f, new FabricItemSettings().group(ModItemGroup.MYTHRIL)));
+    // Mythril Hoe
+    public static final Item MYTHRIL_HOE = registerItem("mythril_hoe",
+            new ModHoeItem(ModToolMaterials.MYTHRIL, 0, 0f, new FabricItemSettings().group(ModItemGroup.MYTHRIL)));
 
     public static Item registerItem(String name, Item item) {
         return Registry.register(Registry.ITEM, new Identifier(TutorialMod.MOD_ID, name), item);
