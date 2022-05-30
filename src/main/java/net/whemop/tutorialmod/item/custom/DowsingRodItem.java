48eee887da8b3215814636c8df4f74a188163787
Whemop <spencenate1230@gmail.com>
2022-05-29 23:12:57 -0400

    Tags and 1.18.2 Tag changes

--- src/main/java/net/whemop/tutorialmod/item/custom/DowsingRodItem.java
+++ src/main/java/net/whemop/tutorialmod/item/custom/DowsingRodItem.java
@@ -13,7 +13,10 @@ import net.minecraft.text.Text;
 import net.minecraft.text.TranslatableText;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.registry.Registry;
 import net.minecraft.world.World;
+import net.whemop.tutorialmod.item.ModItems;
+import net.whemop.tutorialmod.util.ModTags;
 import org.jetbrains.annotations.Nullable;
 
 import java.util.List;
@@ -68,7 +71,6 @@ public class DowsingRodItem extends Item {
     }
 
     private boolean isValuableBlock(Block block) {
-        return block == Blocks.COAL_ORE || block == Blocks.COPPER_ORE
-                || block == Blocks.DIAMOND_ORE || block == Blocks.IRON_ORE;
+        return Registry.BLOCK.getOrCreateEntry(Registry.BLOCK.getKey(block).get()).isIn(ModTags.Blocks.DOWSING_ROD_DETECTABLE_BLOCKS);
     }
 }
